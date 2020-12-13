5354ebe8ddd7b6ea04a28df1e813fb427bf264c7
pankaj7822 <pankaj.krjha.cse17@itbhu.ac.in>
2020-12-13 20:10:55 +0530

    Migrated to Sqlite

--- QuestionBankIOE/settings.py
+++ QuestionBankIOE/settings.py
@@ -81,11 +81,8 @@ WSGI_APPLICATION = 'QuestionBankIOE.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'questionbank',
-        'USER': 'django',
-        'PASSWORD': 'django-pass',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
